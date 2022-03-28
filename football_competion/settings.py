3fca3b56b9240d104f7da7a5cb0aa4fcb519e206
JoelFah <joelfah2003@gmail.com>
2022-03-28 17:06:25 +0100

    Added translation tags in templates

--- football_competion/settings.py
+++ football_competion/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'core/static')
 ]
 
 # Email configuration for gmail smtp
