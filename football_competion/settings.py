75c7ef8126e7d086fbc67c66c35892e6bd45a031
main-c <yannikkwc@gmail.com>
2022-04-02 10:34:40 +0100

    settings deploy

--- football_competion/settings.py
+++ football_competion/settings.py
@@ -67,13 +67,12 @@ WSGI_APPLICATION = 'football_competion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = env(
-    'DATABASES', {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    })
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
